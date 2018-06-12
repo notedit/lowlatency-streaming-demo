64f5e8159bc8b2ed9dec0af2d9bb335749ebf883
notedit <notedit@gmail.com>
2018-06-12 17:36:31 +0800

    add pod

---
+++ avlittle/avlittle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  avlittle
+//
+//  Created by xiang on 12/06/2018.
+//  Copyright © 2018 dotEngine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ avlittle/avlittle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  avlittle
+//
+//  Created by xiang on 12/06/2018.
+//  Copyright © 2018 dotEngine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
